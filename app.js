3fd556b9346e7ed96a2e8aacf504e78cfa93faff
sp1deydev <duckhanhthientran@gmail.com>
2025-02-07 10:18:03 +0700

    add body-parser

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express();
 const morgan = require('morgan');
 require('dotenv').config();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 app.listen(PORT);
